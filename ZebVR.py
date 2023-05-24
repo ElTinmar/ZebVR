28afe564cc668faf07022744495952b6bab1d1ce
ElTinmar <martin.privat@hotmail.fr>
2023-05-24 13:46:37 +0200

    create file

---
+++ ZebVR.py
@@ -0,0 +1,10 @@
+from core.VR import VR
+
+camera = 
+projector = 
+background = 
+cam2proj = 
+tracker = 
+stimulus = 
+
+VirtualReality = VR(camera, projector, background, cam2proj, tracker, stimulus)
