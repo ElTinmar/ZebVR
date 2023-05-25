7e1acfeb997a729cd1efdd83a28a6e4052902dd1
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 17:47:37 +0200

    show tracking

--- core/VR.py
+++ core/VR.py
@@ -1,4 +1,7 @@
 from .protocols import Camera, Projector, Background, Cam2Proj, Tracker, Stimulus
+import cv2
+from tracking.utils.im2gray import im2gray
+from tracking.utils.im2float import im2single
 
 class VR:
     def __init__(
@@ -22,6 +25,7 @@ class VR:
         self.registration()
         self.run()
 
+
     def calibration(self):
         self.camera.calibration()
         self.projector.calibration()
@@ -30,6 +34,8 @@ class VR:
         self.cam2proj.registration()
 
     def run(self):
+
+        cv2.namedWindow('VR')
         self.camera.start_acquisition()
 
         keepgoing = True
@@ -39,9 +45,18 @@ class VR:
                 image = data.get_img()
                 self.background.add_image(image)
                 background_image = self.background.get_background() 
-                tracking = self.tracker.track(image-background_image)
+                back_sub = abs(image - background_image)
+                tracking = self.tracker.track(back_sub)
+                overlay = self.tracker.tracking_overlay(back_sub)
                 stim_image = self.stimulus.create_stim_image(tracking)
                 self.projector.project(stim_image)
                 data.reallocate()
+                
+                for c in range(overlay.shape[2]):
+                    overlay[:,:,c] = overlay[:,:,c] + image
+
+                cv2.imshow('VR', overlay)
+                cv2.waitKey(1)
 
         self.camera.stop_acquisition()
+        cv2.destroyWindow('VR')
