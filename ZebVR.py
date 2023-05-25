6575b7a2dc36c4d6149cb8b87ea43189e7ac72ac
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 09:20:21 +0200

    added background

--- ZebVR.py
+++ ZebVR.py
@@ -1,11 +1,11 @@
 from core.VR import VR
 from devices.camera.dummycam import FromFile, RandomCam
-from core.abstractclasses import Projector
+from core.abstractclasses import Projector, Background
 
 cam_opts = {"ROI_height": 128, "ROI_width": 256}
 camera = RandomCam(**cam_opts)
 projector = Projector(monitor_id = 1)
-background = 
+background = Background(num_images = 500, every_n_image = 100)
 cam2proj = 
 tracker = 
 stimulus = 
