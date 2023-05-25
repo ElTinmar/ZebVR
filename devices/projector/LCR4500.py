e08cdd1006aa8fed962388cced551283a8812f06
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 10:38:45 +0200

    delted and moved files

--- devices/projector/LCR4500.py
+++
@@ -1,8 +0,0 @@
-from .lightcrafter_4500.configure import configure
-from core.abstractclasses import Projector
-
-class LCR4500(Projector):
-    def __init__(self, monitor_id) -> None:
-        super().__init__(monitor_id)
-        configure()
-
