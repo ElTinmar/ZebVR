7c2fe834226313a86f5a15a08802e4508e0e9f59
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 14:22:47 +0200

    transform into a class

--- registration/registration_cam2proj.py
+++ registration/registration_cam2proj.py
@@ -1,85 +1,111 @@
 import numpy as np 
 import cv2
+from numpy.typing import NDArray
+from core.protocols import Camera, Projector
+from typing import List
+import cv2
 
-# TODO that function is big, maybe you can subdivide it further.
-# That could also allow different calibration patterns to be used
 # TODO warn if there is substantial shearing/deformations that 
 # the projector is not well aligned
 
-def calibration(
-    cam, 
-    num_frames_per_pt = 10, 
-    wdw_offset = (0,0),
-    grid_size = (10,10), 
-    proj_size = (1140,912),
-    dot_radius = 5,
-    dot_intensity = 255,
-    ksize = (10,10)
-    ):
-    """
-    Project points on a grid and detect them on camera in order 
-    to obtain an affine registration between camera and projector
-    spaces
-    """
+class Cam2ProjReg:
+    def __init__(
+        self,
+        camera: Camera,
+        projector: Projector,
+        num_frames_per_pt: int = 10, 
+        grid_size_x: int = 10,
+        grid_size_y: int = 10, 
+        dot_radius: int = 5,
+        dot_intensity: int = 255,
+        ksize: int = 10
+    ) -> None:
+        
+        self.camera = camera
+        self.projector = projector
+        self.num_frames_per_pt = num_frames_per_pt
+        self.grid_size_x = grid_size_x
+        self.grid_size_y = grid_size_y
+        self.dot_radius = dot_radius
+        self.dot_intensity = dot_intensity
+        self.ksize = ksize
 
-    cv2.namedWindow("Calibration", cv2.WINDOW_FULLSCREEN)
-    cv2.moveWindow("Calibration", wdw_offset[0], wdw_offset[1])
+        cv2.namedWindow('registration')
 
-    dot = np.zeros(proj_size, dtype=np.uint8)
-    xx,yy = np.meshgrid(range(proj_size[1]),range(proj_size[0]))
+    def registration(self) -> List[NDArray]:
 
-    x_start = round(proj_size[0]/(grid_size[0]+1))
-    x_step = x_start
-    x_stop = (grid_size[0]+1)*x_step
-    y_start = round(proj_size[1]/(grid_size[1]+1))
-    y_step = y_start
-    y_stop = (grid_size[1]+1)*y_step
+        proj_size = self.projector.get_resolution()
+        (width,height) = self.camera.get_resolution()
 
-    height = cam.get_height()
-    width = cam.get_width()
-    image = np.zeros((height*width), dtype=np.single)
+        xx,yy = np.meshgrid(range(proj_size[0]),range(proj_size[1]))
 
-    cam.start_acquisition()
+        x_start = round(proj_size[0]/(self.grid_size_x+1))
+        x_step = x_start
+        x_stop = (self.grid_size_x+1)*x_step
+        y_start = round(proj_size[1]/(self.grid_size_y+1))
+        y_step = y_start
+        y_stop = (self.grid_size_y+1)*y_step
 
-    count = 0
-    coords_cam = np.zeros((grid_size[0]*grid_size[1],2), dtype=np.single)
-    coords_proj = np.zeros((grid_size[0]*grid_size[1],2), dtype=np.single)
-    for x in range(x_start,x_stop,x_step):
-        for y in range(y_start,y_stop,y_step):
+        self.camera.start_acquisition()
 
-            # project a dot at (x,y)
-            indices = (xx-x)**2 + (yy-y)**2 <= dot_radius**2
-            dot[indices] = dot_intensity
-            cv2.imshow(dot)
-            cv2.waitKey()
+        count = 0
+        coords_cam = np.zeros((self.grid_size_x*self.grid_size_y,2), dtype=np.single)
+        coords_proj = np.zeros((self.grid_size_x*self.grid_size_y,2), dtype=np.single)
+        for x in range(x_start,x_stop,x_step):
+            for y in range(y_start,y_stop,y_step):
+                # project a dot at (x,y)
+                indices = (xx-x)**2 + (yy-y)**2 <= self.dot_radius**2
+                dot = np.zeros((proj_size[1],proj_size[0]) , dtype=np.uint8)
+                dot[indices] = self.dot_intensity
+                self.projector.project(dot)
 
-            # average over a few frames
-            for frame in range(num_frames_per_pt):
-                buffer = cam.fetch()
-                img_data = np.ndarray(
-                    (height*width), 
-                    dtype=np.single, 
-                    buffer= 1/256 * buffer.get_img()
-                )
-                image = image + img_data/num_frames_per_pt
-                buffer.queue()
+                # average over a few frames
+                image = np.zeros((height,width), dtype=np.single)
+                for frame in range(self.num_frames_per_pt):
+                    data, res = self.camera.fetch()
+                    img_data = data.get_img()
+                    img_data = img_data.astype(np.single)/255 
+                    image = image + img_data/self.num_frames_per_pt
+                    data.reallocate()
 
-            # detect x,y position on camera sensor
-            image = cv2.blur(image, ksize)
-            x_cam,y_cam = np.unravel_index(np.argmax(image), image.shape)
+                # detect x,y position on camera sensor
+                image = cv2.blur(image, (self.ksize, self.ksize))
+                x_cam,y_cam = np.unravel_index(np.argmax(image), image.shape)
 
-            # store in numpy array
-            coords_proj[count,:] = [x,y]
-            coords_cam[count,:] = [x_cam,y_cam] 
-            count += 1
+                # show detected points
+                detected_spot = cv2.circle(image,(x_cam,y_cam),10,0,1)
+                print(detected_spot)
+                cv2.imshow('registration', detected_spot)
+                cv2.waitKey(1)
+                
+                # store in numpy array
+                coords_proj[count,:] = [x,y]
+                coords_cam[count,:] = [x_cam,y_cam] 
+                count += 1
 
-    cam.stop_acquisition()
+        self.camera.stop_acquisition()
 
-    # Use homogeneous coordinates to get affine transform
-    coords_proj = np.append(coords_proj, np.ones((grid_size[0]*grid_size[1],1)),axis=1)
-    coords_cam = np.append(coords_cam, np.ones((grid_size[0]*grid_size[1],1)),axis=1)
-    Affine_proj2cam, _, _, _ = np.linalg.lstsq(coords_proj,coords_cam)
-    Affine_cam2proj, _, _, _ = np.linalg.lstsq(coords_cam,coords_proj)
+        # Use homogeneous coordinates to get affine transform
+        coords_proj = np.append(
+            coords_proj, 
+            np.ones((self.grid_size_x*self.grid_size_y,1)),
+            axis=1
+        )
+        coords_cam = np.append(
+            coords_cam, 
+            np.ones((self.grid_size_x*self.grid_size_y,1)),
+            axis=1
+        )
+        
+        try:
+            Affine_proj2cam, _, _, _ = np.linalg.lstsq(coords_proj,coords_cam)
+            Affine_cam2proj, _, _, _ = np.linalg.lstsq(coords_cam,coords_proj)
+        except:
+            print("Not enough points for registration, using identity")
+            Affine_proj2cam = np.eye(3)
+            Affine_cam2proj = np.eye(3)
 
-    return Affine_cam2proj, Affine_proj2cam
+        return Affine_cam2proj, Affine_proj2cam
 
+    def __del__(self):
+        cv2.destroyWindow('registration')
