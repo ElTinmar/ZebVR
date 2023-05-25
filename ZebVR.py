02594076697b8698958aa5da5e15d47178d31b7a
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 12:14:47 +0200

    adding parameters

--- ZebVR.py
+++ ZebVR.py
@@ -8,8 +8,6 @@ from tracking.tail.tail_tracker import TailTracker
 from tracking.prey.prey_tracker import PreyTracker
 from tracking.tracker_collection import TrackerCollection 
 
-
-
 cam_opts = {"ROI_height": 128, "ROI_width": 256}
 camera = RandomCam(**cam_opts)
 
@@ -19,12 +17,33 @@ background = Background(num_images = 500, every_n_image = 100)
 
 cam2proj = 
 
-body_tracker = BodyTracker()
-eyes_tracker = EyesTracker()
-tail_tracker = TailTracker()
-prey_tracker = PreyTracker()
+body_tracker = BodyTracker(
+    threshold_body_intensity = 0.2,
+    threshold_body_area = 500
+)
+eyes_tracker = EyesTracker(
+    threshold_body_intensity = 0.2,
+    threshold_body_area = 500,
+    threshold_eye_intensity = 0.4,
+    threshold_eye_area_min = 100,
+    threshold_eye_area_max = 500
+)
+tail_tracker = TailTracker(
+    threshold_body_intensity = 0.2,
+    threshold_body_area = 500,
+    tail_length = 160,
+    n_tail_points = 12,
+    ksize = 5,
+    arc_angle_deg = 150,
+    n_pts_interp = 40,
+    n_pts_arc = 20
+)
+prey_tracker = PreyTracker(
+    threshold_prey_intensity = 0.025,
+    threshold_prey_area_min = 15,
+    threshold_prey_area_max = 100
+)
 full_tracker = TrackerCollection([eyes_tracker, tail_tracker, prey_tracker])
-
 tracker = full_tracker
 
 stimulus = 
