9990486b43f79c060acec8426fd870e4da75a5bd
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 16:25:04 +0200

    update background only when change

--- background/background.py
+++ background/background.py
@@ -1,6 +1,7 @@
 from collections import deque
 from scipy import stats
 from numpy.typing import NDArray
+import cv2
 
 class Background:
     def __init__(self, num_images = 500, every_n_image = 100) -> None:
@@ -8,21 +9,29 @@ class Background:
         self.every_n_image = every_n_image
         self.image_store = deque(maxlen=num_images)
         self.counter = 0
+        self.background = None
+
+        cv2.namedWindow('background')
         
     def add_image(self, image : NDArray) -> None:
         """
-        Input an image to update the background model
+        Input an image and update the background model
         """
-        self.counter += 1
         if self.counter % self.every_n_image == 0:
             self.image_store.append(image)
+            if len(self.image_store) == 1:
+                self.background = self.image_store[0]
+            else:
+                # that seems to be slow
+                self.background = stats.mode(self.image_store, axis=0, keepdims=False).mode
+        self.counter += 1
 
     def get_background(self) -> NDArray:
         """
         Outputs a background image
         """
-        if len(self.image_store) == 1:
-            res = self.image_store[0]
-        else:
-            res = stats.mode(self.image_store, axis=0, keepdims=False).mode
-        return res
+
+        cv2.imshow('background',self.background)
+        cv2.waitKey(1)
+
+        return self.background
