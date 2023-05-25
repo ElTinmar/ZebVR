4a93847c3ccb9100017290db0b6ecc55f06b75d7
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 10:40:35 +0200

    changed imports after renaming

--- ZebVR.py
+++ ZebVR.py
@@ -1,10 +1,11 @@
 from core.VR import VR
 from devices.camera.dummycam import FromFile, RandomCam
-from core.abstractclasses import Projector, Background
+from devices.projector.opencv_projector import CVProjector
+from background.background import Background
 
 cam_opts = {"ROI_height": 128, "ROI_width": 256}
 camera = RandomCam(**cam_opts)
-projector = Projector(monitor_id = 1)
+projector = CVProjector(monitor_id = 1)
 background = Background(num_images = 500, every_n_image = 100)
 cam2proj = 
 tracker = 
