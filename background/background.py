4a632bb18b5d4cf934098e29d393ba20c3e5153c
ElTinmar <martin.privat@hotmail.fr>
2023-05-25 10:38:58 +0200

    create separate file

---
+++ background/background.py
@@ -0,0 +1,24 @@
+from collections import deque
+from scipy import stats
+from numpy.typing import NDArray
+
+class Background:
+    def __init__(self, num_images = 500, every_n_image = 100) -> None:
+        self.num_images = num_images
+        self.every_n_image = every_n_image
+        self.image_store = deque(maxlen=num_images)
+        self.counter = 0
+        
+    def add_image(self, image : NDArray) -> None:
+        """
+        Input an image to update the background model
+        """
+        self.counter += 1
+        if self.counter % self.every_n_image == 0:
+            self.image_store.append(image)
+
+    def get_background(self) -> NDArray:
+        """
+        Outputs a background image
+        """
+        return stats.mode(self.image_store, axis=2, keepdims=False).mode
