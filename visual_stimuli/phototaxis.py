f5173c24030f1f11ef4bcc731da73dc9cb48f88d
ElTinmar <martin.privat@hotmail.fr>
2023-06-01 10:59:36 +0200

    fix out of order frames issue

--- visual_stimuli/phototaxis.py
+++ visual_stimuli/phototaxis.py
@@ -9,16 +9,21 @@ class Phototaxis(Stimulus):
     def __init__(self, projector: Projector):
         super().__init__()
         self.projector = projector
+        self.last_timestamp = 0
 
-    def create_stim_image(self, parameters: TrackingCollection) -> NDArray:
-        width, height = self.projector.get_resolution()
-        xv, yv = np.meshgrid(range(width), range(height), indexing='xy')
-        img = np.zeros((height,width), dtype=np.single)
-        if parameters.body is not None:
-            img = 1.0*(
-                ((xv-parameters.body.centroid[0]) * parameters.body.heading[0,1] + \
-                 (yv-parameters.body.centroid[1]) * parameters.body.heading[1,1]) > 0
-            )
-        return img
+    def create_stim_image(self, timestamp: int, parameters: TrackingCollection) -> NDArray:
+        if timestamp > self.last_timestamp:
+            self.last_timestamp = timestamp
+            width, height = self.projector.get_resolution()
+            xv, yv = np.meshgrid(range(width), range(height), indexing='xy')
+            img = np.zeros((height,width), dtype=np.single)
+            if parameters.body is not None:
+                img = 1.0*(
+                    ((xv-parameters.body.centroid[0]) * parameters.body.heading[0,1] + \
+                    (yv-parameters.body.centroid[1]) * parameters.body.heading[1,1]) > 0
+                )
+            return img
+        else:
+            return None
 
     
