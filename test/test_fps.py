bcb301fe143aed71d7f8768737fa9fa98a5c4bf5
ElTinmar <martin.privat@hotmail.fr>
2023-06-07 17:29:12 +0200

    add file

---
+++ test/test_fps.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import time
+
+X = np.random.rand(1200,1900)
+cv2.namedWindow('test')
+
+def test_fps(fps = 60):
+    counter = 0
+    while counter < 100:
+        start_time_ns = time.process_time_ns()
+        cv2.imshow('test', X)
+        cv2.waitKey(1)
+        counter += 1
+        if 1e-9*(time.process_time_ns()-start_time_ns) > 1/fps:
+            print('too slow')
+        else:
+            while 1e-9*(time.process_time_ns()-start_time_ns) < 1/fps:
+                pass
+
+%timeit test_fps(20)
