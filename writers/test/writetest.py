179f34f6672335791a04698b1dec82bb861955a6
ElTinmar <martin.privat@hotmail.fr>
2023-06-08 12:48:51 +0200

    fixing videowriter

--- writers/test/writetest.py
+++ writers/test/writetest.py
@@ -2,8 +2,10 @@ from writers.jpg import JPG_writer
 from writers.png import PNG_writer
 from writers.tiff import TIFF_writer
 from writers.raw_array import RawArray_writer
+from writers.video import CVWriter
 import numpy as np
 
+# image writers
 prefix = '/home/martin/Documents/test'
 writer_list = [JPG_writer,PNG_writer,RawArray_writer,TIFF_writer]
 
@@ -12,3 +14,17 @@ for W in writer_list:
     for i in range(100):
         X = np.random.rand(1024,1024)
         writer.write(X)
+
+# video writer
+writer = CVWriter(
+    height = 1024,
+    width = 1024,
+    fps = 100,
+    filename = prefix + '/video.avi'
+)
+
+writer.start()
+for i in range(1000):
+    X = np.random.rand(1024,1024)
+    writer.write(X)
+writer.stop()
--- writers/video.py
+++ writers/video.py
@@ -2,6 +2,7 @@ from core.abstractclasses import ImageSaver
 import cv2
 from numpy.typing import NDArray
 import os.path
+import numpy as np
 
 class CVWriter(ImageSaver):
     def __init__(
@@ -23,6 +24,16 @@ class CVWriter(ImageSaver):
         self.width = width
         self.codec = codec
     
+    def to_uint8(self, image: NDArray) -> NDArray:
+        if image.dtype is not np.uint8:
+            rescaled = 255 * (image - image.min())/(image.max() - image.min())
+            return rescaled.astype(np.uint8)
+        else:
+            return image
+        
+    def to_BGR(self, image_gray: NDArray) -> NDArray:
+        return np.dstack((image_gray,image_gray,image_gray))
+        
     def start(self) -> None: 
         fourcc = cv2.VideoWriter_fourcc(*self.codec)
         self.cap = cv2.VideoWriter(
@@ -36,6 +47,8 @@ class CVWriter(ImageSaver):
         self.cap.release()
 
     def write(self, image: NDArray) -> None:
-        self.cap.write(image)
+        image_uint8 = self.to_uint8(image)
+        image_BGR_uint8 = self.to_BGR(image_uint8)
+        self.cap.write(image_BGR_uint8)
 
     
