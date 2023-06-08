2bd15d8755b14acec351a4903027bf4651b91583
ElTinmar <martin.privat@hotmail.fr>
2023-06-08 19:45:01 +0200

    trying stuff

--- plotters/data_plotter.py
+++ plotters/data_plotter.py
@@ -3,16 +3,31 @@ from core.abstractclasses import DataPlotter
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from multiprocessing import Process, Array
 
 class DataPlot(DataPlotter):
     def __init__(self) -> None:
         super().__init__()
         
         self.fig, self.ax = plt.subplots()
-        self.xdata, self.ydata = [], []
-        self.line, = self.ax.plot([], [], 'ro')
-        self.ani = FuncAnimation(self.fig, self.plot, self.format_data,
-                    init_func=self.init_plot, blit=True)
+        self.xdata = Array('d',100) 
+        self.ydata = Array('d',100)
+        self.line, = self.ax.plot(self.xdata, self.ydata, 'ro')
+        self.proc = Process(target=self.animate)
+        
+    def start(self):
+        self.proc.start()
+
+    def stop(self):
+        self.proc.join()
+
+    def animate(self):
+        self.ani = FuncAnimation(
+            self.fig, 
+            self.plot,
+            init_func=self.init_plot, 
+            blit=True
+        )
         plt.show()
 
     def init_plot(self):
@@ -20,24 +35,17 @@ class DataPlot(DataPlotter):
         self.ax.set_ylim(-1, 1)
         return self.line,
 
-    def format_data(self, data: dict):
-        """
-        Receive data as a dict
-        """
-        yield data['x'],data['y']
+    def update(self, x, y):
+        self.xdata[:-1] = self.xdata[1:]
+        self.xdata[:-1] = self.xdata[1:]
+        self.xdata[-1] = x
+        self.ydata[-1] = y
 
-    def plot(self, data) -> None:
-        """
-        Input dictionary with keys legend, x, y
-        """
-        x, y = data
-        self.xdata.append(x)
-        self.ydata.append(y)
+    def plot(self, i) -> None:
         xmin, xmax = self.ax.get_xlim()
-
-        if x >= xmax:
+        if self.xdata[-1] >= xmax:
             self.ax.set_xlim(xmin, 2*xmax)
             self.ax.figure.canvas.draw()
         self.line.set_data(self.xdata, self.ydata)
-
-        return self.line,        
+        return self.line,
+     
---
+++ plotters/test/testplotter.py
@@ -0,0 +1,5 @@
+from plotters.data_plotter import DataPlot
+
+plotter = DataPlot()
+
+plotter.update(1,1)
