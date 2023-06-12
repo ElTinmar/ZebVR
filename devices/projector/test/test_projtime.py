b48b87472bdc2c0eb6fc3a3019fe121fe76ecc01
ElTinmar <martin.privat@hotmail.fr>
2023-06-12 16:47:16 +0200

    trying different image lib

--- devices/projector/test/test_projtime.py
+++ devices/projector/test/test_projtime.py
@@ -8,16 +8,19 @@ color = True
 
 # create movie
 if color:
-    X  = np.random.rand(1024,1024,3,N)
+    X  = np.random.rand(1440,2560,3,N)
 else:
-    X  = np.random.rand(1024,1024,N)
+    X  = np.random.rand(1440,2560,N)
 
-# projector
+# projector opencv 
 projector = CVProjector(monitor_id = 1)
 projector.init_window()
 start_time_ns = time.process_time_ns()
 for i in range(N):
-    projector.project(X[:,:,:,i])
+    if color:
+        projector.project(X[:,:,:,i])
+    else:
+        projector.project(X[:,:,i])
 duration =1e-9*(time.process_time_ns() - start_time_ns)/N
 print(f'projector {duration}')
 projector.close_window()
@@ -26,9 +29,25 @@ projector.close_window()
 cv2.namedWindow('direct')
 start_time_ns = time.process_time_ns()
 for i in range(N):
-    cv2.imshow('direct',X[:,:,:,i])
+    if color:
+        cv2.imshow('direct',X[:,:,:,i])
+    else:
+        cv2.imshow('direct',X[:,:,i])
     cv2.waitKey(1)
 duration = 1e-9*(time.process_time_ns() - start_time_ns)/N
 print(f'direct {duration}')
 cv2.destroyWindow('direct')
 
+# projector psychopy
+from devices.projector.psychopy_projector import PsychoPyProjector
+projector = PsychoPyProjector(monitor_id = 0)
+projector.init_window()
+start_time_ns = time.process_time_ns()
+for i in range(N):
+    if color:
+        projector.project(X[:,:,:,i])
+    else:
+        projector.project(X[:,:,i])
+duration =1e-9*(time.process_time_ns() - start_time_ns)/N
+print(f'projector {duration}')
+projector.close_window()
