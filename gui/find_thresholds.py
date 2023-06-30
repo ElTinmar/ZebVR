6b4436091ec7450cbaefc12828e20240df9b6890
ElTinmar <martin.privat@hotmail.fr>
2023-06-30 10:56:31 +0200

    change directory structure

---
+++ gui/find_thresholds.py
@@ -0,0 +1,6 @@
+# you need camera, background
+# do background subtraction and thresholding + bwareafilter and show resulting masks
+# controls:
+#   - intensity threshold as well as size thresholds for bwareafilter
+# display:
+# current area in pixel of blobs ? Average intensity of blob ?
---
+++ gui/main.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtCore, QtWidgets, QtGui
+from core.abstractclasses import Camera, Background
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs) -> None:
+
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("ZebVR")
+        self.update_interval = 30
+        self.update_timer = QtCore.QTimer()
+        self.update_timer.setInterval(self.update_interval)
+        self.update_timer.timeout.connect(self.update_plots)
+        self.update_timer.start()
+        
+    @QtCore.pyqtSlot()
+    def update_plots(self):
+        """do somethig here"""
