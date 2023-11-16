3e6ab9456ecdc255980b478ec8a8b8fd46c51480
ElTinmar <martin.privat@hotmail.fr>
2023-11-16 13:41:52 +0100

    log total loop time

--- ZebVR/core/worker.py
+++ ZebVR/core/worker.py
@@ -78,12 +78,14 @@ class ZebVR_Worker(ABC):
         receive_time_ms = (t1_ns - t0_ns) * 1e-6
         process_time_ms = (t2_ns - t1_ns) * 1e-6
         send_time_ms = (t3_ns - t2_ns) * 1e-6
+        total_time_ms = (t3_ns - t0_ns) * 1e-6
 
         self.local_logger.debug(f'''
             #{iteration} ,
             receive_time: {receive_time_ms}, 
             process_time: {process_time_ms}, 
-            send_time: {send_time_ms}
+            send_time: {send_time_ms},
+            total_time: {total_time_ms}
         ''')
 
     def initialize(self) -> None:
