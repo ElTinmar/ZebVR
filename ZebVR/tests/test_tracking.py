561f7337624d9bc95f8ff967f30f84a893635ff2
ElTinmar <martin.privat@hotmail.fr>
2024-02-01 18:57:33 +0100

    update

--- ZebVR/tests/test_tracking.py
+++ ZebVR/tests/test_tracking.py
@@ -117,7 +117,9 @@ class Display(WorkerNode):
 
 if __name__ == "__main__":
 
-    m = MovieFileCam(filename='/home/martin/Development/ZebVR/toy_data/freely_swimming_param.avi')
+    PIX_PER_MM = 40  
+
+    m = MovieFileCam(filename='toy_data/freely_swimming_param.avi')
     h, w = (m.get_height(), m.get_width())
 
     o = MultiFishOverlay_opencv(
@@ -133,81 +135,82 @@ if __name__ == "__main__":
         accumulator=None, 
         animal=AnimalTracker_CPU(
             tracking_param=AnimalTrackerParamTracking(
-                animal_contrast=1.0,
-                animal_gamma=1.0,
+                pix_per_mm=PIX_PER_MM,
+                target_pix_per_mm=7.5,
                 animal_intensity=0.07,
                 animal_brightness=0.0,
-                blur_sz_mm=0.13,
-                max_animal_length_mm=12,
-                max_animal_size_mm=30,
-                max_animal_width_mm=2.5,
-                median_filter_sz_mm=0.13,
-                min_animal_length_mm=1,
-                min_animal_size_mm=1,
-                min_animal_width_mm=0.4,
-                pad_value_mm=4,
-                pix_per_mm=40,
-                target_pix_per_mm=7.5
+                animal_gamma=1.0,
+                animal_contrast=1.0,
+                min_animal_size_mm=1.0,
+                max_animal_size_mm=30.0,
+                min_animal_length_mm=0,
+                max_animal_length_mm=0,
+                min_animal_width_mm=0,
+                max_animal_width_mm=0,
+                pad_value_mm=4.0,
+                blur_sz_mm=1/7.5,
+                median_filter_sz_mm=0,
             )
         ),
         body=BodyTracker_CPU(
             tracking_param=BodyTrackerParamTracking(
-                pix_per_mm=40,
-                target_pix_per_mm=7.5,
-                body_intensity=0.06,
-                body_gamma=1.5,
-                body_contrast=3.0,
+                pix_per_mm=PIX_PER_MM,
+                target_pix_per_mm=10,
+                body_intensity=0.20,
                 body_brightness=0.0,
-                min_body_size_mm=0,
-                max_body_size_mm=30,
+                body_gamma=1.0,
+                body_contrast=3.0,
+                min_body_size_mm=0.0,
+                max_body_size_mm=30.0,
                 min_body_length_mm=0,
-                max_body_length_mm=6,
+                max_body_length_mm=0,
                 min_body_width_mm=0,
-                max_body_width_mm=1.2,
-                blur_sz_mm=0.13,
-                median_filter_sz_mm=0.13
+                max_body_width_mm=0,
+                blur_sz_mm=1/7.5,
+                median_filter_sz_mm=0,
             )
         ),
         eyes=EyesTracker_CPU(
             tracking_param=EyesTrackerParamTracking(
-                pix_per_mm=40,
+                pix_per_mm=PIX_PER_MM,
                 target_pix_per_mm=40,
-                eye_gamma=2.5,
-                eye_contrast=3.5,
-                eye_brightness=0.5,
-                eye_dyntresh_res=5,
+                eye_brightness=0.0,
+                eye_gamma=3.0,
+                eye_dyntresh_res=10,
+                eye_contrast=5.0,
                 eye_size_lo_mm=0.8,
-                eye_size_hi_mm=10,
-                crop_dimension_mm=(1.0,1,5),
-                crop_offset_mm=-0.3,
+                eye_size_hi_mm=10.0,
                 blur_sz_mm=0.06,
-                median_filter_sz_mm=0.06
+                median_filter_sz_mm=0,
+                crop_dimension_mm=(1.0,1.5),
+                crop_offset_mm=-0.75
             )
         ),
         tail=TailTracker_CPU(
             tracking_param=TailTrackerParamTracking(
-                pix_per_mm=40,
+                pix_per_mm=PIX_PER_MM,
                 target_pix_per_mm=20,
-                tail_contrast=1,
-                tail_gamma=0.75,
-                tail_brightness=0.0,
-                arc_angle_deg=120,
-                n_tail_points=12,
+                ball_radius_mm=0.05,
+                arc_angle_deg=90,
+                n_tail_points=6,
                 n_pts_arc=20,
                 n_pts_interp=40,
-                tail_length_mm=2.3,
-                crop_offset_tail_mm=2.25,
-                dist_swim_bladder_mm=-0.2,
-                crop_dimension_mm=(3.5,3.5),
+                tail_length_mm=2.2,
+                dist_swim_bladder_mm=0.0,
                 blur_sz_mm=0.06,
-                median_filter_sz_mm=0.06
+                median_filter_sz_mm=0,
+                tail_brightness=0.0,
+                tail_contrast=3.0,
+                tail_gamma=0.75,
+                crop_dimension_mm=(3.5,3.5),
+                crop_offset_tail_mm=1.75
             )
         )
     )
 
     l = Logger('test_tracking.log', Logger.DEBUG)
     b = BackroundImage(
-        image_file_name = '/home/martin/Development/ZebVR/toy_data/freely_swimming_param.png',
+        image_file_name = 'toy_data/freely_swimming_param.png',
         polarity = Polarity.DARK_ON_BRIGHT
     )
     
@@ -247,6 +250,7 @@ if __name__ == "__main__":
             data_type = np.uint8
         )
     )
+    q_display = MonitoredQueue(QueueMP())
     q_tracking = MonitoredQueue(QueueMP())
     q_overlay = MonitoredQueue(QueueMP())
 
