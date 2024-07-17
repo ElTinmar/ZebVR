aa65f81af9e6326957d544c9211ce868fa29c52f
ElTinmar <martin.privat@hotmail.fr>
2024-07-17 10:24:52 +0200

    update

--- ZebVR/calibration/pix_per_mm.py
+++ ZebVR/calibration/pix_per_mm.py
@@ -4,7 +4,7 @@ from ZebVR.config import (
     CAM_WIDTH, CAM_HEIGHT,
     CAM_GAIN, CAM_OFFSETX, CAM_OFFSETY, CAMERA_CONSTRUCTOR,
     CALIBRATION_SQUARE_SIZE_MM, CALIBRATION_CAM_EXPOSURE_MS,
-    CALIBRATION_CAM_FPS 
+    CALIBRATION_CAM_FPS, CALIBRATION_CHECKER_SIZE
 )
 
 if __name__ == '__main__':
@@ -19,17 +19,15 @@ if __name__ == '__main__':
     camera.set_offsetX(CAM_OFFSETX)
     camera.set_offsetY(CAM_OFFSETY)
 
-    checker_sz = (9,6)
-
-    objp = np.zeros((np.prod(checker_sz),3), np.float32)
+    objp = np.zeros((np.prod(CALIBRATION_CHECKER_SIZE),3), np.float32)
     objp[:,:2] = np.mgrid[
-        0:checker_sz[0]*CALIBRATION_SQUARE_SIZE_MM:CALIBRATION_SQUARE_SIZE_MM,
-        0:checker_sz[1]*CALIBRATION_SQUARE_SIZE_MM:CALIBRATION_SQUARE_SIZE_MM
+        0:CALIBRATION_CHECKER_SIZE[0]*CALIBRATION_SQUARE_SIZE_MM:CALIBRATION_SQUARE_SIZE_MM,
+        0:CALIBRATION_CHECKER_SIZE[1]*CALIBRATION_SQUARE_SIZE_MM:CALIBRATION_SQUARE_SIZE_MM
     ].T.reshape(-1,2)
 
     px_per_mm = get_camera_px_per_mm(
         cam=camera, 
-        checkerboard_size=checker_sz, 
+        checkerboard_size=CALIBRATION_CHECKER_SIZE, 
         checkerboard_corners_world_coordinates_mm=objp,  
         camera_matrix=None,
         distortion_coef=None,
