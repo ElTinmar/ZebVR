c9ba48fd6f9841351e030bac625a913782a3324d
ElTinmar <martin.privat@hotmail.fr>
2024-07-17 10:24:05 +0200

    update

--- ZebVR/calibration/pix_per_mm.py
+++ ZebVR/calibration/pix_per_mm.py
@@ -2,27 +2,24 @@ import numpy as np
 from camera_tools import get_camera_px_per_mm
 from ZebVR.config import (
     CAM_WIDTH, CAM_HEIGHT,
-    CAM_GAIN, CAM_FPS,
-    CAM_OFFSETX, CAM_OFFSETY, CAMERA_CONSTRUCTOR,
-    CALIBRATION_SQUARE_SIZE_MM, CALIBRATION_CAM_EXPOSURE_MS
+    CAM_GAIN, CAM_OFFSETX, CAM_OFFSETY, CAMERA_CONSTRUCTOR,
+    CALIBRATION_SQUARE_SIZE_MM, CALIBRATION_CAM_EXPOSURE_MS,
+    CALIBRATION_CAM_FPS 
 )
 
 if __name__ == '__main__':
 
-    CALIBRATION_CAM_EXPOSURE_MS = 20_000
-    CAM_FPS = 5
 
     camera = CAMERA_CONSTRUCTOR()
     camera.set_exposure(CALIBRATION_CAM_EXPOSURE_MS)
     camera.set_gain(CAM_GAIN)
-    camera.set_framerate(CAM_FPS)
+    camera.set_framerate(CALIBRATION_CAM_FPS)
     camera.set_height(CAM_HEIGHT)
     camera.set_width(CAM_WIDTH)
     camera.set_offsetX(CAM_OFFSETX)
     camera.set_offsetY(CAM_OFFSETY)
 
     checker_sz = (9,6)
-    CALIBRATION_SQUARE_SIZE_MM = 3
 
     objp = np.zeros((np.prod(checker_sz),3), np.float32)
     objp[:,:2] = np.mgrid[
