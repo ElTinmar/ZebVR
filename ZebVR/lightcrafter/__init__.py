b2a75565e9d1aa127836f256f2333746f95a6f28
ElTinmar <martin.privat@hotmail.fr>
2024-07-20 12:33:34 +0200

    fix

--- ZebVR/lightcrafter/__init__.py
+++ ZebVR/lightcrafter/__init__.py
@@ -1,2 +1,2 @@
-from lcr_pattern_mode import set_lcr_pattern_mode
-from lcr_video_mode import set_lcr_video_mode
+from .lcr_video_mode import set_lcr_video_mode
+from .lcr_pattern_mode import set_lcr_pattern_mode
