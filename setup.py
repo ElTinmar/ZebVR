bafa6ae59bf22bcb588ee1918e19f45434b2ddd3
ElTinmar <martin.privat@hotmail.fr>
2024-09-04 16:05:21 +0200

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ZebVR',
     python_requires='==3.8',
     author='Martin Privat',
-    version='0.1.2',
+    version='0.1.3',
     packages=['ZebVR', 'ZebVR.background', 'ZebVR.calibration', 'ZebVR.lightcrafter', 'ZebVR.stimulus', 'ZebVR.widgets', 'ZebVR.workers'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='Open and closed-loop virtual reality',
