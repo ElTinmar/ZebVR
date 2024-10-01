6f28d699f0c48bbcf4a0567dbf681d61238770e9
ElTinmar <martin.privat@hotmail.fr>
2024-10-01 14:08:01 +0200

    update

--- ZebVR/projections/cylindrical.py
+++ ZebVR/projections/cylindrical.py
@@ -1,108 +1,72 @@
 import sympy
 import numpy as np
-#sympy.init_printing(use_unicode=True)
 
-x_s, y_s, z_s = sympy.symbols('x_s y_s z_s')
-x_sp, y_sp, z_sp = sympy.symbols('x_sp y_sp z_sp')
+# declare symbols ------------------------------------
+
 x_o, y_o, z_o = sympy.symbols('x_o y_o z_o')
-x_p, y_p, z_p = sympy.symbols('x_p y_p z_p')
 x_f, y_f, z_f = sympy.symbols('x_f y_f z_f')
-d, r, t_1 = sympy.symbols('d r t_1')
-t_0 = sympy.symbols('t_0', nonnegative=True)
+x_s, y_s, z_s = sympy.symbols('x_s y_s z_s')
+t = sympy.symbols('t', nonnegative=True)
 
-# 2D : infinitely tall objects
+r, t_0, t_1, t_2, t_3 = sympy.symbols('r t_0 t_1 t_2 t_3')
 
-eq_circle = sympy.Eq(x_s*x_s + y_s*y_s, r*r)
-eq_line = sympy.Eq((y_s - y_f), ((y_o - y_f)/(x_o - x_f)) * (x_s - x_f))
-eq_thales = sympy.Eq((y_s / (d+x_s)), (y_sp/(d-r)))
-constraint = sympy.Ge((x_o - x_f)*(x_s - x_f) + (y_o - y_f)*(y_s - y_f), 0)
+d0, d1, d2, d3 = sympy.symbols('d d1 d2 d3')
+x_p0, y_p0, z_p0 = sympy.symbols('x_p0 y_p0 z_p0')
+x_p1, y_p1, z_p1 = sympy.symbols('x_p1 y_p1 z_p1')
+x_p2, y_p2, z_p2 = sympy.symbols('x_p2 y_p2 z_p2')
+x_p3, y_p3, z_p3 = sympy.symbols('x_p3 y_p3 z_p3')
+x_sp0, y_sp0, z_sp0 = sympy.symbols('x_sp0 y_sp0 z_sp0')
+x_sp1, y_sp1, z_sp1 = sympy.symbols('x_sp1 y_sp1 z_sp1')
+x_sp2, y_sp2, z_sp2 = sympy.symbols('x_sp2 y_sp2 z_sp2')
+x_sp3, y_sp3, z_sp3 = sympy.symbols('x_sp3 y_sp3 z_sp3')
 
-solutions = sympy.solve([eq_circle, eq_line, eq_thales], [y_sp, y_s, x_s], dict=True)
+# declare equations ------------------------------------
 
-
-def transform(r, d, x_o, y_o, x_f, y_f):
-    y0 = -(-d + r) * (
-        - d*x_f**2*y_o 
-        + d*x_f*x_o*y_f 
-        + d*x_f*x_o*y_o 
-        - d*x_o**2*y_f 
-        + d*y_f*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2) 
-        - d*y_o*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2) 
-        + r**2*x_f*y_f 
-        - r**2*x_f*y_o 
-        - r**2*x_o*y_f 
-        + r**2*x_o*y_o 
-        - x_f*y_o*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2) 
-        + x_o*y_f*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2)
-    ) / (
-        -d**2*x_f**2 
-        + 2*d**2*x_f*x_o 
-        - d**2*x_o**2 
-        - d**2*y_f**2 
-        + 2*d**2*y_f*y_o 
-        - d**2*y_o**2 
-        + 2*d*x_f*y_f*y_o 
-        - 2*d*x_f*y_o**2 
-        - 2*d*x_o*y_f**2 
-        + 2*d*x_o*y_f*y_o 
-        + r**2*x_f**2 
-        - 2*r**2*x_f*x_o 
-        + r**2*x_o**2 
-        - x_f**2*y_o**2 
-        + 2*x_f*x_o*y_f*y_o 
-        - x_o**2*y_f**2
-    )
-    y1 = -(-d + r) * ( 
-        - d*x_f**2*y_o 
-        + d*x_f*x_o*y_f 
-        + d*x_f*x_o*y_o 
-        - d*x_o**2*y_f 
-        - d*y_f*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2) 
-        + d*y_o*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2) 
-        + r**2*x_f*y_f 
-        - r**2*x_f*y_o 
-        - r**2*x_o*y_f 
-        + r**2*x_o*y_o 
-        + x_f*y_o*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2) 
-        - x_o*y_f*np.sqrt(r**2*x_f**2 - 2*r**2*x_f*x_o + r**2*x_o**2 + r**2*y_f**2 - 2*r**2*y_f*y_o + r**2*y_o**2 - x_f**2*y_o**2 + 2*x_f*x_o*y_f*y_o - x_o**2*y_f**2)
-    ) / (
-        - d**2*x_f**2 
-        + 2*d**2*x_f*x_o 
-        - d**2*x_o**2 
-        - d**2*y_f**2 
-        + 2*d**2*y_f*y_o 
-        - d**2*y_o**2 
-        + 2*d*x_f*y_f*y_o 
-        - 2*d*x_f*y_o**2 
-        - 2*d*x_o*y_f**2 
-        + 2*d*x_o*y_f*y_o 
-        + r**2*x_f**2 
-        - 2*r**2*x_f*x_o 
-        + r**2*x_o**2 
-        - x_f**2*y_o**2 
-        + 2*x_f*x_o*y_f*y_o 
-        - x_o**2*y_f**2
-    )
-    return (y0, y1)
-
-x, y = np.mgrid[-20:-10,10:20]
-
-transform(30, 100, x, y, 10, -10)
-
-
-# Full 3D projection 
-
-eq_line_FO_x = sympy.Eq(x_s, x_f + (x_o - x_f) * t_0)
-eq_line_FO_y = sympy.Eq(y_s, y_f + (y_o - y_f) * t_0)
-eq_line_FO_z = sympy.Eq(z_s, z_f + (z_o - z_f) * t_0)
+## object to cylinder projection
+eq_line_FO_x = sympy.Eq(x_s, x_f + (x_o - x_f) * t)
+eq_line_FO_y = sympy.Eq(y_s, y_f + (y_o - y_f) * t)
+eq_line_FO_z = sympy.Eq(z_s, z_f + (z_o - z_f) * t)
 eq_cylinder = sympy.Eq(x_s*x_s + y_s*y_s, r*r)
-eq_line_SP_x = sympy.Eq(x_sp, x_s + (x_p - x_s) * t_1)
-eq_line_SP_y = sympy.Eq(y_sp, y_s + (y_p - y_s) * t_1)
-eq_line_SP_z = sympy.Eq(z_sp, z_s + (z_p - z_s) * t_1)
-eq_screen_plane = sympy.Eq(x_sp, -r)
-eq_proj_x = sympy.Eq(x_p, -d)
-eq_proj_y = sympy.Eq(y_p, 0)
-eq_proj_z = sympy.Eq(z_p, 0)
+
+## cylinder to screen projection
+
+# ----- Projector 0: left
+eq_proj0_x = sympy.Eq(x_p0, -d0)
+eq_proj0_y = sympy.Eq(y_p0, 0)
+eq_proj0_z = sympy.Eq(z_p0, 0)
+eq_line_SP0_x = sympy.Eq(x_sp0, x_s + (x_p0 - x_s) * t_0)
+eq_line_SP0_y = sympy.Eq(y_sp0, y_s + (y_p0 - y_s) * t_0)
+eq_line_SP0_z = sympy.Eq(z_sp0, z_s + (z_p0 - z_s) * t_0)
+eq_screen_plane0 = sympy.Eq(x_sp0, -r)
+
+# ----- Projector 1: top
+eq_proj1_x = sympy.Eq(x_p1, 0)
+eq_proj1_y = sympy.Eq(y_p1, d1)
+eq_proj1_z = sympy.Eq(z_p1, 0)
+eq_line_SP1_x = sympy.Eq(x_sp1, x_s + (x_p1 - x_s) * t_1)
+eq_line_SP1_y = sympy.Eq(y_sp1, y_s + (y_p1 - y_s) * t_1)
+eq_line_SP1_z = sympy.Eq(z_sp1, z_s + (z_p1 - z_s) * t_1)
+eq_screen_plane1 = sympy.Eq(y_sp1, r)
+
+# ----- Projector 2: right
+eq_proj2_x = sympy.Eq(x_p2, d2)
+eq_proj2_y = sympy.Eq(y_p2, 0)
+eq_proj2_z = sympy.Eq(z_p2, 0)
+eq_line_SP2_x = sympy.Eq(x_sp2, x_s + (x_p2 - x_s) * t_2)
+eq_line_SP2_y = sympy.Eq(y_sp2, y_s + (y_p2 - y_s) * t_2)
+eq_line_SP2_z = sympy.Eq(z_sp2, z_s + (z_p2 - z_s) * t_2)
+eq_screen_plane2 = sympy.Eq(x_sp2, r)
+
+# ----- Projector 3: bottom
+eq_proj3_x = sympy.Eq(x_p3, 0)
+eq_proj3_y = sympy.Eq(y_p3, -d3)
+eq_proj3_z = sympy.Eq(z_p3, 0)
+eq_line_SP3_x = sympy.Eq(x_sp3, x_s + (x_p3 - x_s) * t_3)
+eq_line_SP3_y = sympy.Eq(y_sp3, y_s + (y_p3 - y_s) * t_3)
+eq_line_SP3_z = sympy.Eq(z_sp3, z_s + (z_p3 - z_s) * t_3)
+eq_screen_plane3 = sympy.Eq(y_sp3, -r)
+
+# solve equations ------------------------------------
 
 solutions = sympy.solve(
     [
@@ -110,173 +74,39 @@ solutions = sympy.solve(
         eq_line_FO_y,
         eq_line_FO_z,
         eq_cylinder,
-        eq_line_SP_x,
-        eq_line_SP_y,
-        eq_line_SP_z,
-        eq_screen_plane,
-        eq_proj_x,
-        eq_proj_y,
-        eq_proj_z
+        eq_proj0_x,
+        eq_proj0_y,
+        eq_proj0_z,
+        eq_line_SP0_x,
+        eq_line_SP0_y,
+        eq_line_SP0_z,
+        eq_screen_plane0,
+        eq_line_SP1_x,
+        eq_line_SP1_y,
+        eq_line_SP1_z,
+        eq_screen_plane1,
+        eq_line_SP2_x,
+        eq_line_SP2_y,
+        eq_line_SP2_z,
+        eq_screen_plane2,
+        eq_line_SP3_x,
+        eq_line_SP3_y,
+        eq_line_SP3_z,
+        eq_screen_plane3
     ], 
-    [y_sp, z_sp, t_1, x_p, y_p, z_p, z_s, x_sp, x_s, y_s, t_0],
+    [
+      x_sp0, x_sp1, x_sp2, x_sp3,
+      y_sp0, y_sp1, y_sp2, y_sp3,
+      z_sp0, z_sp1, z_sp2, z_sp3,
+      t_0, t_1, t_2, t_3,
+      x_p0, y_p0, z_p0, 
+      x_p1, y_p1, z_p1, 
+      x_p2, y_p2, z_p2, 
+      x_p3, y_p3, z_p3, 
+      z_s, x_s, y_s, t
+    ],
     dict=True
 )
-
-def transform(r, d, x_o, y_o, x_f, y_f):
-    x_sp = -r
-    y_sp = -(-d + r) * (
-        - d*x_f**2*y_o 
-        + d*x_f*x_o*y_f 
-        + d*x_f*x_o*y_o 
-        - d*x_o**2*y_f 
-        - d*y_f*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        ) 
-        + d*y_o*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        ) 
-        + r**2*x_f*y_f 
-        - r**2*x_f*y_o 
-        - r**2*x_o*y_f 
-        + r**2*x_o*y_o 
-        + x_f*y_o*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        ) 
-        - x_o*y_f*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        )
-    ) / (
-        -d**2*x_f**2 
-        + 2*d**2*x_f*x_o 
-        - d**2*x_o**2 
-        - d**2*y_f**2 
-        + 2*d**2*y_f*y_o 
-        - d**2*y_o**2 
-        + 2*d*x_f*y_f*y_o 
-        - 2*d*x_f*y_o**2 
-        - 2*d*x_o*y_f**2 
-        + 2*d*x_o*y_f*y_o 
-        + r**2*x_f**2 
-        - 2*r**2*x_f*x_o 
-        + r**2*x_o**2 
-        - x_f**2*y_o**2 
-        + 2*x_f*x_o*y_f*y_o 
-        - x_o**2*y_f**2
-    )
-    z_sp = -(-d + r) * (
-        -d*x_f**2*z_o 
-        + d*x_f*x_o*z_f 
-        + d*x_f*x_o*z_o 
-        - d*x_o**2*z_f 
-        - d*y_f**2*z_o 
-        + d*y_f*y_o*z_f 
-        + d*y_f*y_o*z_o 
-        - d*y_o**2*z_f 
-        - d*z_f*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        ) 
-        + d*z_o*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        ) 
-        + r**2*x_f*z_f 
-        - r**2*x_f*z_o 
-        - r**2*x_o*z_f 
-        + r**2*x_o*z_o 
-        + x_f*y_f*y_o*z_o 
-        - x_f*y_o**2*z_f 
-        + x_f*z_o*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        ) 
-        - x_o*y_f**2*z_o 
-        + x_o*y_f*y_o*z_f 
-        - x_o*z_f*np.sqrt(
-            r**2*x_f**2 
-            - 2*r**2*x_f*x_o 
-            + r**2*x_o**2 
-            + r**2*y_f**2 
-            - 2*r**2*y_f*y_o 
-            + r**2*y_o**2 
-            - x_f**2*y_o**2 
-            + 2*x_f*x_o*y_f*y_o 
-            - x_o**2*y_f**2
-        )
-    ) / (
-        -d**2*x_f**2 
-        + 2*d**2*x_f*x_o 
-        - d**2*x_o**2 
-        - d**2*y_f**2 
-        + 2*d**2*y_f*y_o 
-        - d**2*y_o**2 
-        + 2*d*x_f*y_f*y_o 
-        - 2*d*x_f*y_o**2 
-        - 2*d*x_o*y_f**2 
-        + 2*d*x_o*y_f*y_o 
-        + r**2*x_f**2 
-        - 2*r**2*x_f*x_o 
-        + r**2*x_o**2 
-        - x_f**2*y_o**2 
-        + 2*x_f*x_o*y_f*y_o 
-        - x_o**2*y_f**2
-    )
-
-    return x_sp, y_sp, z_sp
-
 
 # TODO: additional constraints: 
 # F has to be inside the cylinder
