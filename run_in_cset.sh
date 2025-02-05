c2a0f32867bf11ec7a6da670569dea12a2610dc4
ElTinmar <martin.privat@hotmail.fr>
2025-02-05 17:59:47 +0100

    remove file

--- run_in_cset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cset shield --cpu 2-31 --kthread on
-cset shield --user martinprivat --exec bash -c 'conda activate ZebVR2 && cd /home/martin/Development/ZebVR && python ZebVR/main.py'
-cset shield --reset
