eecbfd8c5759639ee59d671eb081ab002d42eec0
ElTinmar <martin.privat@hotmail.fr>
2025-02-05 16:28:52 +0100

    try affinity

--- ZebVR/dags/__init__.py
+++ ZebVR/dags/__init__.py
@@ -1,5 +1,6 @@
 from .closed_loop import closed_loop
 from .closed_loop_with_triggers import closed_loop_with_triggers
+from .closed_loop_with_affinity import closed_loop_with_affinity
 from .open_loop import open_loop
 from .video_recording import video_recording
 from .tracking import tracking
