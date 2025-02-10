7676779e0853c94878c310baa442124ba72b94ea
ElTinmar <martin.privat@hotmail.fr>
2025-02-10 15:31:10 +0100

    in the process of refactoring

--- ZebVR/widgets/__init__.py
+++ ZebVR/widgets/__init__.py
@@ -1,7 +1,4 @@
-from .stimulus_widget import *
 from .camera_widget import *
-from .display_widget import *
-from .tracking_widget import *
 from .sequencer_widget import *
 from .projector_widget import *
 from .registration_widget import *
@@ -9,4 +6,14 @@ from .calibration_widget import *
 from .background_widget import *
 from .vr_settings_widget import *
 from .output_widget import *
+
+from .close_loop_widget import *
+from .open_loop_widget import *
+from .video_recording_widget import *
+from .tracking_widget import *
+
+from .stimulus_widget import *
+from .display_widget import *
+from .tracking_settings_widget import *
 from .queue_widget import *
+
