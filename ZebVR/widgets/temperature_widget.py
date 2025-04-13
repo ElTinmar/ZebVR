cef38485c8f5e9b32fdad16288343721245fcc2b
ElTinmar <martin.privat@hotmail.fr>
2025-04-13 15:29:16 +0200

    update

--- ZebVR/widgets/temperature_widget.py
+++ ZebVR/widgets/temperature_widget.py
@@ -9,15 +9,20 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import pyqtSignal, QObject, QRunnable, QThreadPool, QTimer
 
-
 from ..serial_utils import list_serial_arduino
 
+# TODO: pyqtgraph with current temperature and target zone
 class TemperatureWidget(QWidget):
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args, **kwargs)
         self.current_temperature: float = 0
 
+    def set_current_temperature(self, temp: float) -> None:
+        self.current_temperature = temp
+
+# TODO: don't modify QWidget directly here, use QTimer in TemperatureWidget
+# and set temperature of the widge
 class TemperatureMonitor(QRunnable):
     pass
 
