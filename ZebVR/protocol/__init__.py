8fa4099ce22ba8b5fa37e09df676fc17e3907abb
ElTinmar <martin.privat@hotmail.fr>
2025-06-02 23:36:45 +0200

    widget dict

--- ZebVR/protocol/__init__.py
+++ ZebVR/protocol/__init__.py
@@ -1,2 +1,17 @@
 from .protocol_item import *
 from .stop_condition import *
+from .visual import *
+
+PROTOCOL_WIDGETS = {
+    'bright_widget': visual.BrightWidget,
+    'concentric_grating_widget': visual.ConcentricGratingWidget,
+    'dark_widget': visual.DarkWidget,
+    'dot_widget': visual.DotWidget,
+    'following_dot_widget': visual.FollowingDotWidget,
+    'looming_widget': visual.LoomingWidget,
+    'following_looming_widget': visual.FollowingLoomingWidget,
+    'okr_widget': visual.OKR_Widget,
+    'omr_widget': visual.OMR_Widget,
+    'phototaxis_widget': visual.PhototaxisWidget,
+    'prey_capture_widget': visual.PreyCaptureWidget
+}
---
+++ ZebVR/protocol/visual/__init__.py
@@ -0,0 +1,11 @@
+from .bright import Bright, BrightWidget
+from .brightness_ramp import BrightnessRamp, BrightnessRampWidget
+from .concentric_grating import ConcentricGrating, ConcentricGratingWidget
+from .dark import Dark, DarkWidget
+from .dot import Dot, FollowingDot, DotWidget, FollowingDotWidget
+from .image import Image, ImageWidget
+from .looming import Looming, FollowingLooming, LoomingWidget, FollowingLoomingWidget
+from .okr import OKR, OKR_Widget
+from .omr import OMR, OMR_Widget
+from .phototaxis import Phototaxis, PhototaxisWidget
+from .prey_capture import PreyCapture, PreyCaptureWidget
