5f925cac251c732fb485a1e0452ca06fae7c44eb
ElTinmar <martin.privat@hotmail.fr>
2025-07-01 11:20:46 +0200

    added skeleton

--- ZebVR/workers/daq.py
+++ ZebVR/workers/daq.py
@@ -1,1 +1,28 @@
-# TODO 
+from dagline import WorkerNode
+from numpy.typing import NDArray
+from typing import Dict, Optional
+from daq_tools import Arduino_SoftTiming, LabJackU3_SoftTiming, NI_SoftTiming
+
+# TODO work in prog
+class DAQ_Worker(WorkerNode):
+
+    def __init__(
+            self, 
+            *args, 
+            **kwargs
+        ):
+
+        super().__init__(*args, **kwargs)
+
+    def initialize(self) -> None:
+        super().initialize()
+        
+    def cleanup(self) -> None:
+        super().cleanup()
+    
+    def process_data(self, data) -> NDArray:
+        pass
+
+    def process_metadata(self, metadata: Dict) -> Optional[Dict]:
+        pass
+        
