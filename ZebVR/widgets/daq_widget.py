cc0fd4f4448c88d827973f3b9dae2a40baa53c96
ElTinmar <martin.privat@hotmail.fr>
2025-07-01 11:05:35 +0200

    added skeleton code

--- ZebVR/widgets/daq_widget.py
+++ ZebVR/widgets/daq_widget.py
@@ -1,1 +1,41 @@
-# TODO 
+from PyQt5.QtWidgets import (
+    QWidget, 
+    QVBoxLayout
+)
+from PyQt5.QtCore import pyqtSignal
+from typing import Dict
+from qt_widgets import LabeledSpinBox
+
+class DaqWidget(QWidget):
+
+    state_changed = pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.create_components()
+        self.layout_components()
+
+    def create_components(self):
+        pass
+        
+    def layout_components(self) -> None:
+        
+        layout = QVBoxLayout(self)
+        layout.addStretch()
+
+    def get_state(self) -> Dict:
+
+        state = {}
+        return state
+    
+    def set_state(self, state: Dict) -> None:
+
+        setters = {
+        }
+
+        for key, setter in setters.items():
+            if key in state:
+                setter(state[key])
+
+        
