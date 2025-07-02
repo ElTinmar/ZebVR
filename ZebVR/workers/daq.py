58012a320fd4f9e24aa57d59c9dc96bdbc9bf9da
ElTinmar <martin.privat@hotmail.fr>
2025-07-02 10:48:58 +0200

    fix

--- ZebVR/workers/daq.py
+++ ZebVR/workers/daq.py
@@ -21,15 +21,14 @@ class DAQ_Worker(WorkerNode):
 
     def initialize(self) -> None:
 
-        super().initialize()
         self.arduinos = {board_id: Arduino_SoftTiming(board_id) for board_id in self.arduino_IDs}
         self.labjacks = {board_id: LabJackU3_SoftTiming(board_id) for board_id in self.labjack_IDs}
         self.nis = {board_id: NI_SoftTiming(board_id) for board_id in self.national_instruments_IDs}
+        
+        super().initialize()
 
     def cleanup(self) -> None:
         
-        super().cleanup()
-
         for arduino in self.arduinos.values():
             arduino.close()
     
@@ -38,6 +37,8 @@ class DAQ_Worker(WorkerNode):
     
         for ni in self.nis.values():
             ni.close()
+
+        super().cleanup()
 
     def process_data(self, data: Dict) -> NDArray:
         pass
@@ -66,27 +67,28 @@ class DAQ_Worker(WorkerNode):
                         result[name].append((board_id, operation, args, method(*args)))
 
             return result
-        
+
+
 if __name__ == '__main__':
 
-    from dagline import ProcessingDAG, send_strategy, receive_strategy
+    from dagline import ProcessingDAG, EmptyNode, send_strategy, receive_strategy
     from ipc_tools import QueueMP
     from multiprocessing_logger import Logger
     import time
+    from multiprocessing import Process, set_start_method
 
-    class NullNode(WorkerNode):
-
-        def process_data(self, data):
-            pass
-
-        def process_metadata(self, metadata):
-            pass
-
-    dag = ProcessingDAG()
+    set_start_method('spawn')
 
     worker_logger = Logger('daq_worker.txt', Logger.INFO)
     queue_logger = Logger('daq_queue.txt', Logger.INFO)
-
+    
+    # create worker nodes
+    source = EmptyNode(        
+        name = 'source',
+        logger = worker_logger, 
+        logger_queues = queue_logger,
+        send_metadata_strategy = send_strategy.BROADCAST,
+    )
     daq_worker = DAQ_Worker(
         arduino_IDs = [board.id for board in Arduino_SoftTiming.list_boards()],
         labjack_IDs = [board.id for board in LabJackU3_SoftTiming.list_boards()],
@@ -95,24 +97,21 @@ if __name__ == '__main__':
         logger = worker_logger, 
         logger_queues = queue_logger,
         send_metadata_strategy = send_strategy.DISPATCH,
-        receive_metadata_strategy = receive_strategy.POLL
+        receive_metadata_strategy = receive_strategy.POLL,
     )
-    source = NullNode(        
-        name = 'source',
-        logger = worker_logger, 
-        logger_queues = queue_logger,
-        #send_metadata_strategy = send_strategy.DISPATCH
-    )
-    sink = NullNode(        
+    sink = EmptyNode(        
         name = 'sink',
         logger = worker_logger, 
         logger_queues = queue_logger,
-        #receive_metadata_strategy = receive_strategy.POLL
+        receive_metadata_strategy = receive_strategy.POLL,
     )
 
+    # create queues
     input_queue = QueueMP()
     output_queue = QueueMP()
 
+    # create DAG
+    dag = ProcessingDAG()
     dag.connect_metadata(
         sender = source, 
         receiver = daq_worker, 
@@ -126,29 +125,40 @@ if __name__ == '__main__':
         name = 'daq_output'
     )
 
+    # start DAG
+    p_worker_logger = Process(target=worker_logger.run)
+    p_queue_logger = Process(target=queue_logger.run)
+    p_worker_logger.start()
+    p_queue_logger.start()
     dag.start()
+
+    # highjack queues for testing
     input_queue.put(
         {
-            #'arduino': [
-            #    ('/dev/ttyACM0', 'digital_write', (4, True)),
-            #    ('/dev/ttyACM0', 'pwm_write', (9, 0.25))
-            #],
-            #'labjack': [
-            #    (320043003, 'digital_read', (0,)),
-            #    (320043003, 'pwm_write', (4, 0.5)),
-            #    (320043003, 'pwm_write', (5, 0.15))
-            #],
-            'ni': [
-                (0, 'digital_write', (0, True))
-            ]
+            'arduino': [
+                ('/dev/ttyACM0', 'digital_write', (4, True)),
+                ('/dev/ttyACM0', 'pwm_write', (9, 0.25))
+            ],
+            'labjack': [
+                (320043003, 'digital_read', (0,)),
+                (320043003, 'pwm_write', (4, 0.5)),
+                (320043003, 'pwm_write', (5, 0.15))
+            ],
+            #'ni': [
+            #    (0, 'digital_write', (0, True))
+            #]
         }
     )
-    #print(output_queue.get()) # doesnt work with metadata 
+    print(output_queue.get()) 
     time.sleep(1)
+    print(output_queue.qsize())
+    print(input_queue.qsize())
 
-    #input_queue.cancel_join_thread()
-    #output_queue.cancel_join_thread()
-
+    # stop DAG
     dag.stop()
-        
+    worker_logger.stop()
+    queue_logger.stop()
+    p_worker_logger.join()
+    p_queue_logger.join()
+
 
