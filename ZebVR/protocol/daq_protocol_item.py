d48e16c5a8c898a15b7708acbc4bcdec36fe8596
ElTinmar <martin.privat@hotmail.fr>
2025-07-03 19:34:42 +0200

    update

--- ZebVR/protocol/daq_protocol_item.py
+++ ZebVR/protocol/daq_protocol_item.py
@@ -1,20 +1,16 @@
-from .protocol_item import ProtocolItem, ProtocolItemWidget
-from .default import DEFAULT
+
 from typing import Tuple, Dict, Union, List
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import (
     QLabel,
     QHBoxLayout,
-    QComboBox,
-    QApplication
 )
 
-from qt_widgets import LabeledDoubleSpinBox
+from qt_widgets import LabeledComboBox
+from .default import DEFAULT
+from .protocol_item import ProtocolItem, ProtocolItemWidget
 from ..utils import set_from_dict
 from daq_tools import (
-    Arduino_SoftTiming, 
-    LabJackU3_SoftTiming, 
-    NI_SoftTiming, 
     BoardInfo,
     BoardType
 )
@@ -31,6 +27,7 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
         ) -> None:
 
         self.daq_boards = daq_boards
+        self.board_types = list(daq_boards.keys())
 
         super().__init__(*args, **kwargs)
 
@@ -39,50 +36,41 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
         super().declare_components()
 
         # Combobox with board type
-        self.daq_combobox = QComboBox()
-        for board_type in self.daq_boards.keys():
-            self.daq_combobox.addItem(board_type)
-        self.daq_combobox.currentItemChanged.connect()
+        self.daq_combobox = LabeledComboBox()
+        self.daq_combobox.setText('DAQ Board')
+        for board_type in self.board_types:
+            self.daq_combobox.addItem(str(board_type))
+        self.daq_combobox.currentIndexChanged.connect(self.on_board_change)
+
+    def on_board_change(self):
+        self.state_changed.emit()
 
     def layout_components(self) -> None:
 
         super().layout_components()
-
-        self.main_layout.addWidget(self.sb_amplitude_dB)
+        self.main_layout.addWidget(self.daq_combobox)
 
     def get_state(self) -> Dict:
 
         state = super().get_state()
-        state['amplitude_dB'] = self.sb_amplitude_dB.value()
+        state['board_type'] = self.board_types[self.daq_combobox.currentIndex()]
         return state
 
     def set_state(self, state: Dict) -> None:
 
         super().set_state(state)
         
+        # not sure how to do that properly or if that even makes sense?
         set_from_dict(
             dictionary = state,
-            key = 'amplitude_dB',
-            setter = self.sb_amplitude_dB.setValue,
+            key = 'board_type',
+            setter = self.daq_combobox.setCurrentIndex,
             default = self.amplitude_dB,
             cast = float
         )
 
     def from_protocol_item(self, protocol_item: ProtocolItem) -> None:
-        self.sb_amplitude_dB.setValue(protocol_item.amplitude_dB)
+        self.daq_combobox.setValue(protocol_item.board_type)
 
     def to_protocol_item(self) -> ProtocolItem:
         ...
-
-if __name__ == '__main__':
-
-    daq_boards = {
-        BoardType.ARDUINO: Arduino_SoftTiming.list_boards(), 
-        BoardType.LABJACK: LabJackU3_SoftTiming.list_boards(), 
-        BoardType.NATIONAL_INSTRUMENTS: NI_SoftTiming.list_boards()
-    }
-
-    app = QApplication([])
-    widget = DAQ_ProtocolItemWidget(daq_boards)
-    widget.show()
-    app.exec_()
