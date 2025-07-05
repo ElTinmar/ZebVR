123d78a03fad71dda5927393cbcdfcf5e796ab72
ElTinmar <martin.privat@hotmail.fr>
2025-07-05 12:00:56 +0200

    fix names

--- ZebVR/protocol/daq_protocol_item.py
+++ ZebVR/protocol/daq_protocol_item.py
@@ -22,7 +22,7 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
     def __init__(
             self,
             daq_boards: Dict[BoardType, List[BoardInfo]],
-            daq_type: BoardType = DEFAULT['daq_board_type'],
+            daq_board_type: BoardType = DEFAULT['daq_board_type'],
             daq_board_id: Union[str, int] = DEFAULT['daq_board_id'],
             *args,
             **kwargs
@@ -30,7 +30,7 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
 
         self.daq_boards = daq_boards
         self.board_types = list(daq_boards.keys())
-        self.daq_type = daq_type
+        self.daq_board_type = daq_board_type
         self.daq_board_id = daq_board_id
 
         super().__init__(*args, **kwargs)
@@ -40,12 +40,12 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
         super().declare_components()
 
         # Combobox with board type
-        self.daq_type_cb = LabeledComboBox()
-        self.daq_type_cb.setText('DAQ Board Type')
+        self.daq_board_type_cb = LabeledComboBox()
+        self.daq_board_type_cb.setText('DAQ Board Type')
         for board_type in self.board_types:
-            self.daq_type_cb.addItem(str(board_type))
-        self.daq_type_cb.currentIndexChanged.connect(self.on_board_type_change)
-        self.current_board_type = self.board_types[self.daq_type_cb.currentIndex()]
+            self.daq_board_type_cb.addItem(str(board_type))
+        self.daq_board_type_cb.currentIndexChanged.connect(self.on_board_type_change)
+        self.current_board_type = self.board_types[self.daq_board_type_cb.currentIndex()]
         
         # board ID
         self.daq_board_id_cb = LabeledComboBox()
@@ -57,7 +57,7 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
 
     def on_board_type_change(self):
 
-        self.current_board_type = self.board_types[self.daq_type_cb.currentIndex()]
+        self.current_board_type = self.board_types[self.daq_board_type_cb.currentIndex()]
         self.daq_board_id_cb.clear()
         for board in self.daq_boards[self.current_board_type]:
             self.daq_board_id_cb.addItem(str(board.id))
@@ -73,14 +73,14 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
     def layout_components(self) -> None:
 
         super().layout_components()
-        self.main_layout.addWidget(self.daq_type_cb)
+        self.main_layout.addWidget(self.daq_board_type_cb)
         self.main_layout.addWidget(self.daq_board_id_cb)
 
     def get_state(self) -> Dict:
 
         state = super().get_state()
-        state['daq_type'] = self.current_board_type
-        state['board_id'] = self.current_board.id
+        state['daq_board_type'] = self.current_board_type
+        state['daq_board_id'] = self.current_board.id
         return state
 
     def set_state(self, state: Dict) -> None:
@@ -89,15 +89,15 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
         
         set_from_dict(
             dictionary = state,
-            key = 'daq_type',
-            setter = self.daq_type_cb.setCurrentText,
-            default = self.daq_type,
+            key = 'daq_board_type',
+            setter = self.daq_board_type_cb.setCurrentText,
+            default = self.daq_board_type,
             cast = str
         )
 
         set_from_dict(
             dictionary = state,
-            key = 'board_id',
+            key = 'daq_board_id',
             setter = self.daq_board_id_cb.setCurrentText,
             default = self.daq_board_id,
             cast = str
@@ -106,7 +106,7 @@ class DAQ_ProtocolItemWidget(ProtocolItemWidget):
     def from_protocol_item(self, protocol_item: ProtocolItem) -> None:
         
         if isinstance(protocol_item, DAQ_ProtocolItem):
-            self.daq_type_cb.setCurrentText(str(protocol_item.daq_type))
+            self.daq_board_type_cb.setCurrentText(str(protocol_item.daq_board_type))
             self.daq_board_id_cb.setCurrentText(str(protocol_item.daq_board_id))
 
     def to_protocol_item(self) -> DAQ_ProtocolItem:
