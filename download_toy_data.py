27b3d2743bd979b065f316ae1f7fd7e001f34d23
ElTinmar <martin.privat@hotmail.fr>
2025-07-07 13:26:48 +0200

    added download hook

--- download_toy_data.py
+++ download_toy_data.py
@@ -1,13 +1,29 @@
 from urllib.request import urlretrieve
 import zipfile
 import os
+import sys
 
 FOLDER_URL = 'https://owncloud.gwdg.de/index.php/s/qkoXus2Ez28WJ86/download'
 DESTINATION = 'toy_data.zip'
 
 def download() -> None:
+
+    def download_progress_hook(block_num, block_size, total_size):
+        if total_size <= 0:
+            # Total size unknown
+            sys.stdout.write(f"\rDownloaded {block_num * block_size * 1/(1024**2):.2f} MB")
+            sys.stdout.flush()
+        else:
+            downloaded = block_num * block_size
+            percent = min(100, downloaded * 100 / total_size)
+            bar_length = 50
+            filled_length = int(bar_length * percent // 100)
+            bar = '=' * filled_length + '-' * (bar_length - filled_length)
+            sys.stdout.write(f'\rDownloading: |{bar}| {percent:5.1f}%')
+            sys.stdout.flush()
+
     print('Downloading toy data...')
-    urlretrieve(FOLDER_URL, DESTINATION)
+    urlretrieve(FOLDER_URL, DESTINATION, reporthook=download_progress_hook)
 
 def extract() -> None:
     print(f'Extracting {DESTINATION}...')
