5b92524261e2c93014fbac5f09cb5780a3941835
ElTinmar <martin.privat@hotmail.fr>
2025-08-29 19:05:53 +0200

    added file

---
+++ ZebVR/widgets/light_analysis.py
@@ -0,0 +1,37 @@
+from PyQt5.QtWidgets import (
+    QApplication, 
+    QWidget, 
+    QVBoxLayout, 
+    QHBoxLayout, 
+    QLabel, 
+    QPushButton, 
+    QCheckBox,
+    QGroupBox
+)
+from PyQt5.QtCore import pyqtSignal, QObject, QThread, QTimer
+from qt_widgets import LabeledDoubleSpinBox, LabeledSpinBox, LabeledSliderSpinBox, LabeledComboBox
+from typing import Dict, List
+
+from thorlabs_ccs import TLCCS
+
+class LightAnalysisWidget(QWidget):
+
+    def __init__(self,*args,**kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.declare_components()
+        self.layout_components()
+    
+    def declare_components(self) -> None:
+        ...
+
+    def layout_components(self) -> None:
+        ...
+
+if __name__ == '__main__':
+
+    app = QApplication([])
+    window = LightAnalysisWidget()
+    window.show()
+    app.exec()
