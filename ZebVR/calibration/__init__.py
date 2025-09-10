5fcabb9b8e34c55e3a20968524a44e5769dbee53
ElTinmar <martin.privat@hotmail.fr>
2025-09-10 17:16:41 +0200

    added power calibration

--- ZebVR/calibration/__init__.py
+++ ZebVR/calibration/__init__.py
@@ -2,3 +2,4 @@ from .check_registration import check_registration
 from .check_pix_per_mm import check_pix_per_mm
 from .pix_per_mm import pix_per_mm
 from .registration import registration
+from .power_calibration import power_calibration
---
+++ ZebVR/calibration/power_calibration.py
@@ -0,0 +1,201 @@
+from vispy import gloo, app
+from typing import Tuple, Callable
+import sys
+from multiprocessing import Process, RawArray
+import time
+from thorlabs_pmd import TLPMD, Bandwidth, LineFrequency
+import numpy as np
+
+VERT_SHADER_CALIBRATION = """
+attribute vec2 a_position;
+attribute vec4 a_color;
+varying vec4 v_color;
+
+void main()
+{
+    gl_Position = vec4(a_position, 0.0, 1.0);
+    v_color = a_color;
+} 
+"""
+
+FRAG_SHADER_CALIBRATION = """
+varying vec4 v_color;
+
+vec3 linear_to_srgb(vec3 linear)
+{
+    vec3 a = 12.92 * linear;
+    vec3 b = 1.055 * pow(linear, vec3(1.0 / 2.4)) - 0.055;
+    return mix(a, b, step(0.0031308, linear));
+}
+vec4 linear_to_srgb(vec4 linear)
+{
+    return vec4(linear_to_srgb(linear.rgb), linear.a);
+}
+
+void main()
+{
+    gl_FragColor = linear_to_srgb(v_color);
+}
+"""
+
+class Projector(app.Canvas, Process):
+
+    def __init__(
+            self, 
+            window_size: Tuple[int, int] = (1280, 720),
+            window_position: Tuple[int, int] = (0,0),
+            window_decoration: bool = False,
+            fullscreen: bool = True,
+            *args,
+            **kwargs
+        ) -> None:
+            
+            Process.__init__(self, *args, **kwargs)
+            
+            self.window_size = window_size
+            self.window_position = window_position
+            self.window_decoration = window_decoration 
+            self._fullscreen_flag = fullscreen
+            self.color = RawArray('f', [0,0,0,1])
+
+    def initialize(self):
+        # this needs to happen in the process where the window is displayed
+
+        app.Canvas.__init__(
+            self, 
+            size = self.window_size, # (width, height)
+            decorate = self.window_decoration, 
+            position = self.window_position,
+            fullscreen = self._fullscreen_flag, 
+            keys='interactive'
+        )
+
+        self.program = gloo.Program(VERT_SHADER_CALIBRATION, FRAG_SHADER_CALIBRATION)
+        self.program['a_position'] = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+        self.program['a_color'] = self.color[:]
+
+        self.timer = app.Timer('auto', self.on_timer)
+        self.timer.start()
+        self.show()
+
+    def on_timer(self, event):
+        self.program['a_color'] = self.color[:]
+        self.update()
+
+    def on_draw(self, event):
+        gloo.clear('black')
+        self.program.draw('triangle_strip')
+
+    def set_color(self, color: Tuple[float, float, float, float]):
+        self.color[:] = color
+
+    def run(self):
+        self.initialize()
+        if sys.flags.interactive != 1:
+            app.run()
+    
+def power_calibration(
+        powermeter_constructor: Callable[[], TLPMD],
+        proj_width: int,
+        proj_height: int,
+        proj_pos: Tuple[int, int],
+        calibration_file: str,
+        bandwidth: Bandwidth = Bandwidth.LOW,
+        attenuation_dB: float = 0,
+        range_decade: int = -2,
+        average_count: int = 100,
+        line_freq: LineFrequency = LineFrequency.FITFTY_HZ, 
+        wavelength_red: float = 610,
+        wavelength_green: float = 520,
+        wavelength_blue: float = 400,
+        num_steps: int = 11, 
+        pause: float = 1
+    ) -> None:
+
+    color_steps = np.linspace(0,1,num_steps)
+    red_cal = np.zeros((num_steps,), dtype=np.float32)
+    green_cal = np.zeros((num_steps,), dtype=np.float32)
+    blue_cal = np.zeros((num_steps,), dtype=np.float32)
+
+    # set up projector
+    proj = Projector(
+        window_size = (proj_width, proj_height), 
+        window_position = proj_pos, 
+    )
+    proj.start()
+
+    # set up powermeter
+    powermeter = powermeter_constructor()
+
+    # TODO set parameters
+    powermeter.set_bandwidth(bandwidth)
+    powermeter.set_attenuation_dB(attenuation_dB)
+    powermeter.set_current_range_decade(range_decade)
+    powermeter.set_average_count(average_count)
+    powermeter.set_line_frequency_Hz(line_freq)
+
+    # calibrate RGB separately
+    powermeter.set_wavelength_nm(wavelength_red)
+    for i,r in enumerate(color_steps):
+        color = (r,0,0,1.0)
+        proj.set_color(color)
+        time.sleep(pause)
+        red_cal[i] = powermeter.get_power_density_mW_cm2()
+
+    powermeter.set_wavelength_nm(wavelength_green)
+    for i,g in enumerate(color_steps):
+        color = (0,g,0,1.0)
+        proj.set_color(color)
+        time.sleep(pause)
+        green_cal[i] = powermeter.get_power_density_mW_cm2()
+
+    powermeter.set_wavelength_nm(wavelength_blue)
+    for i,b in enumerate(color_steps):
+        color = (0,0,b,1.0)
+        proj.set_color(color)
+        time.sleep(pause)
+        blue_cal[i] = powermeter.get_power_density_mW_cm2()
+
+    proj.terminate()
+    powermeter.close()
+
+    # save results
+    np.savez(
+        calibration_file, 
+        color_steps = color_steps,
+        red_cal = red_cal,
+        green_cal = green_cal,
+        blue_cal = blue_cal
+    )
+
+if __name__ == "__main__":
+
+    from functools import partial
+    from thorlabs_pmd import list_powermeters
+    import matplotlib.pyplot as plt
+
+    powermeters = list_powermeters()
+    powermeter_constructor = partial(TLPMD, device_info=powermeters[0])
+    filename = 'test.npz'
+
+    power_calibration(
+        powermeter_constructor = powermeter_constructor,
+        proj_width = 600,
+        proj_height = 600,
+        proj_pos = (0,0),
+        calibration_file = filename,
+        range_decade = -4,
+        wavelength_red = 610,
+        wavelength_green = 520,
+        wavelength_blue = 400,
+        num_steps = 21, 
+        pause = 2
+    )
+
+    data = np.load(filename)
+    plt.plot(100*data['color_steps'], data['red_cal'], color='r')
+    plt.plot(100*data['color_steps'], data['green_cal'], color='g')
+    plt.plot(100*data['color_steps'], data['blue_cal'], color='b')
+    plt.xlabel("Brightness [%]")
+    plt.ylabel("Irradiance [mW/cm2]")
+    plt.show()
