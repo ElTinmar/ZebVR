d95d84d18fd718cc7875847611141e9ed2947d0d
ElTinmar <martin.privat@hotmail.fr>
2025-09-25 17:26:42 +0200

    tracking fixed hight

--- ZebVR/widgets/display_widget.py
+++ ZebVR/widgets/display_widget.py
@@ -228,9 +228,9 @@ class TrackingDisplayWidget(QWidget):
 
         # TODO make that an argument or controlled by a widget?    
         width = int(image.shape[1] * self.display_height/image.shape[0])
-        #image_resized = cv2.resize(image, (width, self.display_height), interpolation=cv2.INTER_NEAREST)
+        image_resized = cv2.resize(image, (width, self.display_height), interpolation=cv2.INTER_NEAREST)
 
-        pixmap = NDarray_to_QPixmap(image)
+        pixmap = NDarray_to_QPixmap(image_resized)
 
         self.image_item.setPixmap(pixmap)
         if width != self.current_width:
