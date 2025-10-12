c2bf723c724afdca5e46a7685c2d4f8394784587
ElTinmar <martin.privat@hotmail.fr>
2025-10-12 19:26:45 +0200

    also returnn cls attributes

--- ZebVR/utils/serialize.py
+++ ZebVR/utils/serialize.py
@@ -1,6 +1,21 @@
 from typing import Callable, Any, Dict
 from collections import deque
 
+def get_attributes(obj: Any) -> Dict[str, Any]:
+    result = {}
+
+    # Start with class attributes (exclude callables and special names)
+    cls = obj.__class__
+    for k, v in cls.__dict__.items():
+        if not k.startswith("_") and not callable(v):
+            result[k] = v
+
+    # Overlay instance attributes
+    if hasattr(obj, "__dict__"):
+        result.update(obj.__dict__)
+
+    return result
+
 def serialize(obj: Any, serializers: Dict[Any, Callable]):
 
     for t, func in serializers.items():
@@ -16,8 +31,8 @@ def serialize(obj: Any, serializers: Dict[Any, Callable]):
     if isinstance(obj, (str, int, float, bool)) or obj is None:
         return obj
         
-    if hasattr(obj, "__dict__"):
-        if obj.__dict__:
-            return serialize(obj.__dict__, serializers)
+    attrs = get_attributes(obj)
+    if attrs:
+        return serialize(attrs, serializers)
 
     return repr(obj)
