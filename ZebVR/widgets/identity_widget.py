84185b701b41e1096d8896d8abca8988d6a47d06
ElTinmar <martin.privat@hotmail.fr>
2026-07-05 23:56:28 +0200

    load background if present

--- ZebVR/widgets/identity_widget.py
+++ ZebVR/widgets/identity_widget.py
@@ -237,6 +237,7 @@ class IdentityWidget(QWidget):
     
     def set_state(self, state: Dict) -> None:
         self.viewer.set_state(state)
+        self.background_image = state['background']
         
     def on_auto(self):
         modal = FindCircularArenasDialog(image=self.image, pix_per_mm=self.pix_per_mm)
