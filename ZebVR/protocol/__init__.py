33cff47058242d582bdbfb8f3057499105628f8a
ElTinmar <martin.privat@hotmail.fr>
2026-07-17 20:30:28 +0200

    put registry in separate file

--- ZebVR/protocol/__init__.py
+++ ZebVR/protocol/__init__.py
@@ -1,4 +1,3 @@
-from typing import List, Tuple, Type
 from .stim import *
 from .protocol_item import *
 from .visual_protocol_item import *
@@ -9,34 +8,4 @@ from .visual import *
 from .acoustic import *
 from .daq import *
 from .default import *
-
-PROTOCOL_WIDGETS: List[Tuple[Type[ProtocolItemWidget], Stim]] = [
-    (visual.DarkWidget, Stim.DARK),
-    (visual.BrightWidget, Stim.BRIGHT),
-    (visual.PhototaxisWidget, Stim.PHOTOTAXIS),
-    (visual.OKR_Widget, Stim.OKR),
-    (visual.OMR_Widget, Stim.OMR),
-    (visual.DotWidget, Stim.DOT),
-    (visual.LoomingWidget, Stim.LOOMING),
-    (visual.PreyCaptureWidget, Stim.PREY_CAPTURE),
-    (visual.ConcentricGratingWidget, Stim.CONCENTRIC_GRATING),
-    (visual.RampWidget, Stim.RAMP),
-    (visual.ImageWidget, Stim.IMAGE),
-    (visual.TuringWidget, Stim.TURING),
-    (acoustic.PureToneWidget, Stim.PURE_TONE),
-    (acoustic.WhiteNoiseWidget, Stim.WHITE_NOISE),
-    (acoustic.PinkNoiseWidget, Stim.PINK_NOISE),
-    (acoustic.BrownNoiseWidget, Stim.BROWN_NOISE),
-    (acoustic.ClickTrainWidget, Stim.CLICK_TRAIN),
-    (acoustic.FrequencyRampWidget, Stim.FREQUENCY_RAMP),
-    (acoustic.SilenceWidget, Stim.SILENCE),
-    (acoustic.AudioFileWidget, Stim.AUDIO_FILE),
-    (daq.AnalogPulseWidget, Stim.ANALOG_PULSE),
-    (daq.AnalogWriteWidget, Stim.ANALOG_WRITE),
-    (daq.DigitalPulseWidget, Stim.DIGITAL_PULSE),
-    (daq.DigitalWriteWidget, Stim.DIGITAL_WRITE),
-    (daq.PWM_PulseWidget, Stim.PWM_PULSE),
-    (daq.PWM_WriteWidget, Stim.PWM_WRITE),
-    (CompositeProtocolItemWidget, Stim.COMPOSITE)
-]
-PROTOCOL_WIDGETS.sort(key = lambda x: x[1])
+from .registry import PROTOCOL_WIDGETS
---
+++ ZebVR/protocol/registry.py
@@ -0,0 +1,35 @@
+from typing import List, Tuple, Type
+from . import visual, acoustic, daq
+from .stim import Stim
+from .protocol_item import ProtocolItemWidget
+from typing import List, Tuple, Type
+
+PROTOCOL_WIDGETS: List[Tuple[Type[ProtocolItemWidget], Stim]] = [
+    (visual.DarkWidget, Stim.DARK),
+    (visual.BrightWidget, Stim.BRIGHT),
+    (visual.PhototaxisWidget, Stim.PHOTOTAXIS),
+    (visual.OKR_Widget, Stim.OKR),
+    (visual.OMR_Widget, Stim.OMR),
+    (visual.DotWidget, Stim.DOT),
+    (visual.LoomingWidget, Stim.LOOMING),
+    (visual.PreyCaptureWidget, Stim.PREY_CAPTURE),
+    (visual.ConcentricGratingWidget, Stim.CONCENTRIC_GRATING),
+    (visual.RampWidget, Stim.RAMP),
+    (visual.ImageWidget, Stim.IMAGE),
+    (visual.TuringWidget, Stim.TURING),
+    (acoustic.PureToneWidget, Stim.PURE_TONE),
+    (acoustic.WhiteNoiseWidget, Stim.WHITE_NOISE),
+    (acoustic.PinkNoiseWidget, Stim.PINK_NOISE),
+    (acoustic.BrownNoiseWidget, Stim.BROWN_NOISE),
+    (acoustic.ClickTrainWidget, Stim.CLICK_TRAIN),
+    (acoustic.FrequencyRampWidget, Stim.FREQUENCY_RAMP),
+    (acoustic.SilenceWidget, Stim.SILENCE),
+    (acoustic.AudioFileWidget, Stim.AUDIO_FILE),
+    (daq.AnalogPulseWidget, Stim.ANALOG_PULSE),
+    (daq.AnalogWriteWidget, Stim.ANALOG_WRITE),
+    (daq.DigitalPulseWidget, Stim.DIGITAL_PULSE),
+    (daq.DigitalWriteWidget, Stim.DIGITAL_WRITE),
+    (daq.PWM_PulseWidget, Stim.PWM_PULSE),
+    (daq.PWM_WriteWidget, Stim.PWM_WRITE),
+]
+PROTOCOL_WIDGETS.sort(key = lambda x: x[1])
